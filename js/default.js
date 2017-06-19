c5b7cc378d0160fb0d864304f99182b14d7618f0
miyabooks <pompomyamoto@yahoo.co.jp>
2017-06-19 17:33:18 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,11 +6,11 @@ var message;
 if(answer === number) {
   message = 'あたり！';
 }else if(answer < number){
-  message = '残念！もっと大きいです';
+  message = '残念！もっと大きいゾ';
 }else if (number < answer){
-  message = '残念！もっと小さいです';
+  message = '残念！もっと小さいゾ';
 }else{
-  message = '0~3の数字を入力してください';
+  message = '0~3の数字を入力してください！お願いします！何でもしますから！(何でもするとは言ってない)';
 }
 
 //結果を表示
